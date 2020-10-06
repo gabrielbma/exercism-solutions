2a8eb044cb9767b0c85cf24a92fb1cf3b6f6d307
Gabriel Bezerra de Menezes Armelin <gabrielbma@gmail.com>
2020-10-06 02:19:53 +0200

    Changed implementation to use generators for the python/protein exercise.

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,19 +1,32 @@
-import itertools
+from itertools import takewhile, islice
 
 
-def proteins(strand):
+def proteins(rna_seq):
     """Translates RNA sequences into proteins.
+
+    The implementation uses generators and thus would, in theory, be used with a huge chain of codons. However, because it has to return a  list object this generator is consumed completed which might a lot of memory with long chains of codens. Ideally, it would return a generator.
 
     Parameters
     ----------
-    strand (string):
+    rna_seq (string):
         A RNA sequence of codons, each codon is a sequence of 3 nucleotides.
 
     Returns
     -------
     list
-        a list of proteins (polypeptide).
+        a list representing a protein (polypeptide).
     """
+    codon_indexes = get_codon_indexes(rna_seq)
+    codons = (rna_seq[i : i + 3] for i in codon_indexes)
+    protein = (get_amino_acid(codon) for codon in codons)
+    return list(takewhile(lambda x: x != "STOP", protein))
+
+
+def get_codon(rna_seq, codon_index):
+    return rna_seq[codon_index : codon_index + 3]
+
+
+def get_amino_acid(codon):
     codon_to_protein = {
         "AUG": "Methionine",
         "UUU": "Phenylalanine",
@@ -33,6 +46,11 @@ def proteins(strand):
         "UAG": "STOP",
         "UGA": "STOP",
     }
-    indexes = range(len(strand))[::3]
-    proteins = map(lambda i: codon_to_protein[strand[i : i + 3]], indexes)
-    return list(itertools.takewhile(lambda x: x != "STOP", proteins))
+    return codon_to_protein[codon]
+
+
+def get_codon_indexes(rna_seq):
+    i = 0
+    while i < len(rna_seq):
+        yield i
+        i += 3
